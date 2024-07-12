176ac8d3f0c68335413bc5d99ece65b93957169b
Humairaosama9298 <faryalfilza@gmail.com>
2024-07-13 01:40:18 +0500

    update

---
+++ Exercise_02/main.ts
@@ -0,0 +1,14 @@
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+
+
+
+// Exercise_02
+
+
+// store person name in a variable
+let personName: string = "Humaira";
+
+
+// print a message for person
+console.log(`"Hello ${personName}, would you like to learn some Python today?"`);
---
+++ Exercise_03/main.js
@@ -0,0 +1,11 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// Exercise_03
+// person name store in a variable
+let person_name = "Humaira Osama";
+// print person name in lowercase
+console.log(person_name.toLowerCase());
+// print person name in uppercase
+console.log(person_name.toUpperCase());
+// // print person name in uppercase
+console.log(person_name.charAt(0).toUpperCase() + person_name.slice(1).toLowerCase());
+export {};
